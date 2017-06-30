50ea9dfb919ad08475e16390170c9ed4615e1d89
Alexis Irias <lexirias@gmail.com>
2017-06-29 21:04:37 -0600

    Assignment 7
    Can you please let me know why my hamburger nav isn’t working on click.
    Thanks!!

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+	function toggleMenu(){
+		var displayValue = $("nav").css("display");
+		if(displayValue === "none"){
+			$("nav").slideDown();
+		}	else {
+			$("nav").slideUp();
+		}
+	}
+
+	$(".hamburger").click(toggleMenu);
+
+});
